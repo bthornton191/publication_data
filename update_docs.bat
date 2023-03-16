184ab6b8a6944d562f32c8e34b06f24674aa25fc
Ben Thornton <ben.thornton@hexagon.com>
2023-03-16 06:04:27 -0400

    automated copying the rpt files to docs in update_docs.bat

--- update_docs.bat
+++ update_docs.bat
@@ -1,3 +1,4 @@
+robocopy using_machine_learning_to_identify_dem_soil_properties_from_bevameter_test_data\rpt rst\source\_static\using_machine_learning_to_identify_dem_soil_properties_from_bevameter_test_data\rpt /e /mir
 cd rst
 call make html
 cd ..
